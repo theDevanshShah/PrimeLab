40fc5d0e95aac4c822cb0d9bf1c8aa981f78031c
Mr. Devansh Shah <devansh.shah.de@gmail.com>
2026-05-24 18:45:31 +0530

    Solve Q19-Q25 — for loops, range, nested loops, and patterns

--- 02-Python-Fundamentals-2/notes/07_practice_loops.py
+++ 02-Python-Fundamentals-2/notes/07_practice_loops.py
@@ -1,53 +1,72 @@
-# Practice Examples — Loops
+# # Practice Examples — Loops
 
-# 1. Print numbers from 1 to 10
-i = 1
-while i <= 10:
-    print(i, end=" ")
-    i += 1
-print()  # newline
+# # 1. Print numbers from 1 to 10
+# i = 1
+# while i <= 10:
+#     print(i, end=" ")
+#     i += 1
+# print()  # newline
 
-# 2. Print even numbers from 2 to 20
-i = 2
-while i <= 20:
-    print(i, end=" ")
-    i += 2
-print()
+# # 2. Print even numbers from 2 to 20
+# i = 2
+# while i <= 20:
+#     print(i, end=" ")
+#     i += 2
+# print()
 
-# 3. Sum of digits of a number
-num = int(input("Enter a number: "))
-total = 0
-temp = num
+# # 3. Sum of digits of a number
+# num = int(input("Enter a number: "))
+# total = 0
+# temp = num
 
-while temp > 0:
-    total += temp % 10
-    temp //= 10
+# while temp > 0:
+#     total += temp % 10
+#     temp //= 10
 
-print("Sum of digits of", num, "=", total)
+# print("Sum of digits of", num, "=", total)
 
-# 4. Check if a number is palindrome
-num = int(input("Enter a number: "))
-original = num
-rev = 0
+# # 4. Check if a number is palindrome
+# num = int(input("Enter a number: "))
+# original = num
+# rev = 0
 
-while num > 0:
-    digit = num % 10
-    rev = rev * 10 + digit
-    num //= 10
+# while num > 0:
+#     digit = num % 10
+#     rev = rev * 10 + digit
+#     num //= 10
 
-if original == rev:
-    print(original, "is a Palindrome")
-else:
-    print(original, "is NOT a Palindrome")
+# if original == rev:
+#     print(original, "is a Palindrome")
+# else:
+#     print(original, "is NOT a Palindrome")
 
-# 5. Power of a number (without **)
-base = int(input("Enter base: "))
-exp = int(input("Enter exponent: "))
-result = 1
-i = 1
+# # 5. Power of a number (without **)
+# base = int(input("Enter base: "))
+# exp = int(input("Enter exponent: "))
+# result = 1
+# i = 1
 
-while i <= exp:
-    result *= base
-    i += 1
+# while i <= exp:
+#     result *= base
+#     i += 1
 
-print(f"{base}^{exp} = {result}")
+# print(f"{base}^{exp} = {result}")
+
+
+# WAP to print this pattern 
+
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+for i in range(1,5):
+    for j in range(1, 5):
+        print("*",end = " ")
+    print()
+
+for i in range(1,6):
+    for j in range(1, i + 1):
+        print("*",end = " ")
+    print()
